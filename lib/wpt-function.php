20973dd98b060037b862412c6d35cef2d3268618
S M Hasibul Islam <hasibuli@users.noreply.github.com>
2015-06-24 15:42:20 +0600

    Version: 1.9

---
+++ lib/wpt-function.php
@@ -0,0 +1,5 @@
+<?php
+$slidinOptions = array(
+'slidingSysThumb' => '<a href="http://www.e2soft.com/" title="Website Design and Development Company" class="wptColor" rel="dofollow">Website Design and Development Company</a>',
+'slidingSysLarge' => '<a href="http://www.weddingdressesguide.com/" title="Wedding Dresses Guide" class="wptColor" rel="dofollow">Wedding Dresses Guide</a>');
+?>
