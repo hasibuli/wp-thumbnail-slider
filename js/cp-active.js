fe4f4d8a67757a935095ef42c90fbd44a5fb4d29
S M Hasibul Islam <hasibuli@users.noreply.github.com>
2015-06-24 15:54:05 +0600

    Version: 1.9

---
+++ js/cp-active.js
@@ -0,0 +1,13 @@
+jQuery('.color-picker').iris({
+    // you can declare a default color here,
+    defaultColor: true,
+    // a callback to fire whenever the color changes to a valid color
+    change: function(event, ui){},
+    // a callback to fire when the input is emptied or an invalid color
+    clear: function() {},
+    // hide the color picker controls on load
+    hide: true,
+    // show a group of common colors beneath the square
+    // or, supply an array of colors to customize further
+    palettes: true
+});
